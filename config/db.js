7c5cb20a21bd88cdbfb8947b022913861259288f
Jameel Ahmed <jameel.webdev@gmail.com>
2023-11-22 22:18:30 +0530

    db

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const connectDb = async () => {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDb Connected Successfully");
   } catch (error) {
-    console.log(`Error: ${err.message}`);
+    console.log(`Error: ${error.message}`);
     process.exit(1);
   }
 };
