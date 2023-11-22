4c1cbd00525668d832d346fd688e94f18149e646
Jameel Ahmed <jameel.webdev@gmail.com>
2023-11-22 22:28:27 +0530

    STRING

--- config/db.js
+++ config/db.js
@@ -1,8 +1,5 @@
 import mongoose from "mongoose";
-import { config } from "dotenv";
-config({
-  path: "./config.env", // instead of just config()-if path provided we dont need to stop and restart the server once any changes made in .env file
-});
+
 const connectDb = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
